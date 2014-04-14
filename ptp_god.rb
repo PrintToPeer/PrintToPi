eb69a7f500a16d7d9894fed86ce9b4a54cfca4b5
Kaz Walker <KazW@users.noreply.github.com>
2014-04-14 13:59:55 -0700

    Fixes #46 Change location of god's PID files

--- ptp_god.rb
+++ ptp_god.rb
@@ -1,5 +1,12 @@
+require 'fileutils'
+
 HOME        = '/home/pi'
 CONFIG_FILE = "#{HOME}/ptp-config.yml"
+PID_DIR     = "/tmp/PrintToPeer/pids"
 
-God.pid_file_directory = '/var/PrintToPeer/pids'
+FileUtils.mkdir_p PID_DIR
+FileUtils.chmod 770, "/tmp/PrintToPeer"
+FileUtils.chmod 770, PID_DIR
+
+God.pid_file_directory = PID_DIR
 God.load '/etc/ptp_god/*.rb'
