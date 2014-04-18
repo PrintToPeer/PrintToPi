d7ad3bff2e9d874fd20fdcfc040ae3b1a6ccc06f
James Thorne <james@printtopeer.com>
2014-04-18 12:31:43 -0600

    Gave God its own folder (to resolve permissions issues)

--- ptp_god.rb
+++ ptp_god.rb
@@ -2,10 +2,10 @@ require 'fileutils'
 
 HOME        = '/home/pi'
 CONFIG_FILE = "#{HOME}/ptp-config.yml"
-PID_DIR     = "/tmp/PrintToPeer/pids"
+PID_DIR     = "/tmp/PrintToPeer_God/pids"
 
 FileUtils.mkdir_p PID_DIR
-FileUtils.chmod 770, "/tmp/PrintToPeer"
+FileUtils.chmod 770, "/tmp/PrintToPeer_God"
 FileUtils.chmod 770, PID_DIR
 
 God.pid_file_directory = PID_DIR
