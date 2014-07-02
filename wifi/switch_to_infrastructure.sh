b1e66d5e4dea39a3acf7c126665dd9febc4e1753
James Thorne <james@printtopeer.com>
2014-07-02 19:05:43 +0000

    Cleanup

--- wifi/switch_to_infrastructure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo killall dhcpd
-
-sudo cp infrastructure.conf /etc/wpa_supplicant/wpa_supplicant.conf
-sudo wpa_cli reconfigure
-
--- wifi/switch_to_none.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp none.conf /etc/wpa_supplicant/wpa_supplicant.conf
-sudo wpa_cli reconfigure
-
-sudo reboot
