3811625b68ac912dbc71c02e37ae91d416456e62
James Thorne <james@jamesthorne.ca>
2014-07-28 12:50:45 -0600

    Tweaked autoupdate process for improved reliability

--- update/pyserial/update.py
+++ update/pyserial/update.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-import serial
 import subprocess
 
 print "Checking pyserial-2.7..."
 
-if serial.VERSION != '2.7':
+try:
+  import serial
+  assert(serial.VERSION == '2.7')
+except:
   subprocess.Popen('/home/pi/PrintToPi/update/pyserial/update.sh')
-
--- update/pyserial/update.sh
+++ update/pyserial/update.sh
@@ -2,12 +2,14 @@
 
 echo Updating pyserial-2.7...
 
+sudo mount / -o remount,rw
 cd /home/pi/PrintToPi/update/pyserial
 rm -r pyserial-2.7/
 tar xzvf pyserial-2.7.tar.gz
 cd pyserial-2.7
 python setup.py build
 sudo python setup.py install
+sudo mount / -o remount
 
 cd -
 
