b042f6c8a1e0fd7cadb972cafbb90637fe80899c
James Thorne <james@jamesthorne.ca>
2014-08-11 14:34:20 -0600

    Moved Burijji update into PrintToPi to keep them in sync better

---
+++ update/burijji/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/pi/PrintToPi
+LOCAL_TAG=`git describe --abbrev=0 --tags`
+
+cd /home/pi/Burijji
+BURIJJI_TAG=`git describe --abbrev=0 --tags`
+
+echo "Checking Burijji Version: PrintToPi is '$LOCAL_TAG', Burijji is '$BURIJJI_TAG'"
+
+if [ "$BURIJJI_TAG" != "$LOCAL_TAG" ]; then
+  cd /ro/home/pi/Burijji
+
+  sudo mount / -o remount,rw
+  git pull origin master
+  git pull origin master --tags
+  git checkout tags/$LOCAL_TAG
+  sudo mount / -o remount
+fi
--- update/update_all.sh
+++ update/update_all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /home/pi/PrintToPi/update/pyserial/update.py
+/home/pi/PrintToPi/update/burijji/update.sh
