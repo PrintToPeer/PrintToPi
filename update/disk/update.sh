f3e17481bf96a804d679e35b96fe10e89dad1a3c
James Thorne <james@jamesthorne.ca>
2014-08-28 08:37:10 -0600

    - Enabled camera update script - Increased aggressiveness of apt cache fix - Updated to v11

--- update/disk/update.sh
+++ update/disk/update.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-sudo rm -f /var/cache/apt/*
-
-if ls -l /var/cache | grep "w" | grep -q " apt"; then
+if ls -l /var/cache | grep -q " apt"; then
   echo "Fixing apt cache permissions"
   sudo mount / -o remount,rw
-  sudo chmod ugo-w /ro/var/cache/apt
+  sudo rm -rf /ro/var/cache/apt
+  sudo rm -rf /var/cache/apt
   sudo mount / -o remount
-
-  sudo chmod ugo-w /var/cache/apt
 else
   echo "Apt cache permissions OK"
 fi
